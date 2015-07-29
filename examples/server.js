ee5588e9838bc89066b9a7a20069369fb0654114
Arseniy Sluchevskiy <arseniy.sluchevskiy@gmail.com>
2015-07-29 17:46:43 +0300

    proxy loading

--- examples/server.js
+++ examples/server.js
@@ -1,4 +1,8 @@
 var ProxyServer = require('../lib/proxy-server');
 var options = {
+    //proxy: '127.0.0.1:9050',
+    proxy: null,
+    proxyList: './proxy.list',
+    proxyListReloadTimeout: 60 // one minute
 };
 ProxyServer.createServer(options).listen(8080);
--- index.js
+++ index.js
@@ -3,3 +3,5 @@ var ProxyServer = require('./lib/proxy-server');
 module.exports.createServer = ProxyServer.createServer;
 module.exports.buildConnectListener = ProxyServer.buildConnectListener;
 module.exports.buildRequestListener = ProxyServer.buildRequestListener;
+module.exports.getProxyObject = ProxyServer.getProxyObject;
+module.exports.parseProxyLine = ProxyServer.parseProxyLine;
