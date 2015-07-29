78064620b76920d9ae8fdba76615de309eca4215
Arseniy Sluchevskiy <arseniy.sluchevskiy@gmail.com>
2015-07-29 14:56:39 +0300

    ProxyServer class

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 var http = require('http');
 var ProxyServer = require('./lib/proxy-server');
 
-// todo: create ProxyServer class based on http.Server
-
-http.createServer(ProxyServer.requestListener)
-    .on('connect', ProxyServer.connectListener)
-    .listen(8080);
+var options = {}; //todo
+ProxyServer.createServer(options).listen(8080);
